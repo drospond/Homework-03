f69f2d617eb934fe5b49234f5e63e85fec429f60
drospond <david.rospond@gmail.com>
2020-03-06 16:14:17 -0500

    Made generatePassword() functional

--- script.js
+++ script.js
@@ -7,33 +7,64 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 var passLength;
-var useNumeric = false;
-var useUppercase = false;
-var useLowercase = false;
-var useSpecial = false;
+var useNumeric;
+var useUppercase;
+var useLowercase;
+var useSpecial;
 var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var letters = "abcdefghijklmnopqrstuvwxyz";
+var numericCharacters = "1234567890";
 
-function generatePassword(){
-  var randomPassword = "Place Holder";
+function generatePassword() {
+  var randomPassword = "";
+  var availableCharacters = "";
+
   passLength = prompt("How long do you want your password to be? (8-128)");
-  numeric = confirm("Do you want to use numeric characters?");
-  uppercase = confirm("Do you want to use uppercase letters characters?");
-  lowercase = confirm("Do you want to use lowercase letters? characters?");
-  special = confirm("Do you want to use special characters?");
+  if (passLength >= 8 && passLength <= 128) {
+    useNumeric = confirm("Do you want to use numeric characters?");
+    useUppercase = confirm("Do you want to use uppercase letters characters?");
+    useLowercase = confirm("Do you want to use lowercase letters? characters?");
+    useSpecial = confirm("Do you want to use special characters?");
 
-  for(var i = 1; i <= passLength; i++){
-    var charType = Math.floor(Math.random() * 4);
-    console.log(charType);
+    if (useNumeric === true) {
+      availableCharacters += numericCharacters;
+    }
+    if (useUppercase === true) {
+      availableCharacters += letters.toUpperCase();
+    }
+    if (useLowercase === true) {
+      availableCharacters += letters;
+    }
+    if (useSpecial === true) {
+      availableCharacters += specialCharacters;
+    }
+
+    if (
+      useNumeric === true ||
+      useLowercase === true ||
+      useUppercase === true ||
+      useSpecial === true
+    ) {
+      for (var i = 1; i <= passLength; i++) {
+        randomPassword +=
+          availableCharacters[
+            Math.floor(Math.random() * availableCharacters.length)
+          ];
+      }
+    } else {
+      alert("You need atleast one type of character for your password!")
+    }
+  } else {
+    alert("Invalid input. Password needs to be 8-128 characters!");
   }
 
-  console.log(specialCharacters);
+  console.log(randomPassword.length);
+
   return randomPassword;
 }
